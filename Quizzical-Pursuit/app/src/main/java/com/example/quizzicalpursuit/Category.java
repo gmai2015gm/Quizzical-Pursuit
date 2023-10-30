3d4de629caee197b75ffb98961205272e615676a
John <bennet17@gmail.com>
2023-10-30 19:54:11 -0400

    Added Category class, and testing adding to my branch.

---
+++ Quizzical-Pursuit/app/src/main/java/com/example/quizzicalpursuit/Category.java
@@ -0,0 +1,36 @@
+package com.example.quizzicalpursuit;
+
+public class Category {
+    String name;
+
+    int id;
+
+    public Category(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
