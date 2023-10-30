934abbf0a557546d471cd837eb37acbd37cc3aaf
Bennet17 <70712859+Bennet17@users.noreply.github.com>
2023-10-30 13:48:59 -0400

    Add files via upload
    API Logic

---
+++ Quizzical-Pursuit/app/src/main/java/com/example/quizzicalpursuit/Question.java
@@ -0,0 +1,69 @@
+package com.example.quizzical_pursuit;
+
+public class Question {
+
+    String question;
+    String correctAnswer;
+    String incorrectAnswer1;
+    String incorrectAnswer2;
+    String incorrectAnswer3;
+
+    public Question(String question, String correctAnswer, String incorrectAnswer1, String incorrectAnswer2, String incorrectAnswer3) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.incorrectAnswer1 = incorrectAnswer1;
+        this.incorrectAnswer2 = incorrectAnswer2;
+        this.incorrectAnswer3 = incorrectAnswer3;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getIncorrectAnswer1() {
+        return incorrectAnswer1;
+    }
+
+    public void setIncorrectAnswer1(String incorrectAnswer1) {
+        this.incorrectAnswer1 = incorrectAnswer1;
+    }
+
+    public String getIncorrectAnswer2() {
+        return incorrectAnswer2;
+    }
+
+    public void setIncorrectAnswer2(String incorrectAnswer2) {
+        this.incorrectAnswer2 = incorrectAnswer2;
+    }
+
+    public String getIncorrectAnswer3() {
+        return incorrectAnswer3;
+    }
+
+    public void setIncorrectAnswer3(String incorrectAnswer3) {
+        this.incorrectAnswer3 = incorrectAnswer3;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "question='" + question + '\'' +
+                ", correctAnswer='" + correctAnswer + '\'' +
+                ", incorrectAnswer1='" + incorrectAnswer1 + '\'' +
+                ", incorrectAnswer2='" + incorrectAnswer2 + '\'' +
+                ", incorrectAnswer3='" + incorrectAnswer3 + '\'' +
+                '}';
+    }
+}
