60d179699350ecfdd8be500b230e40ba2a3ad13c
gwaldenb <110639045+gwaldenb@users.noreply.github.com>
2023-11-07 19:41:25 -0500

    SO! This is the code to add favorites and for the data to get stored in the sharedpreferences, I will work on sorting tomorrow.

--- Quizzical-Pursuit/app/src/main/java/com/example/quizzicalpursuit/CategoryAdapter.java
+++ Quizzical-Pursuit/app/src/main/java/com/example/quizzicalpursuit/CategoryAdapter.java
@@ -4,6 +4,7 @@ import static androidx.core.content.ContextCompat.startActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,8 +24,11 @@ import java.util.*;
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
 
     ArrayList<Category> category;
+    ArrayList<Integer> faveList;
     Context context;
     int pos;
+    SharedPreferences sp;
+
 
     public CategoryAdapter(Context context,ArrayList<Category> category) {
         this.category = category;
@@ -42,6 +47,36 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
 //        pos = position;
         Category c = category.get(position);
         holder.txtName.setText(""+c.name);
+
+
+        try{
+            String keeper = sp.getString("fa"," ");
+            String holderArray[] = keeper.split(" ");
+            for(int k = 0;k<holderArray.length;k++){
+                faveList.add(Integer.parseInt(holderArray[k]));
+            }
+
+            if(faveList.contains(category.get(position).getId())){
+                holder.btnFav.setImageResource(R.drawable.h1);
+            }else{
+                holder.btnFav.setImageResource(R.drawable.h2);
+            }
+
+            HashSet<Integer> set = new HashSet<>();
+            List<Integer> result = new ArrayList<>();
+
+            for (Integer item : faveList) {
+                if (set.add(item)) {
+                    result.add(item);
+                }
+            }
+
+            faveList.clear();
+            faveList.addAll(result);
+
+        }catch(Exception e){
+            //Log.d("HESH",e+"");
+        }
     }
 
     @Override
@@ -53,15 +88,20 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     class CategoryViewHolder extends RecyclerView.ViewHolder{
         TextView txtName;
         Button btnSelect;
-
+        ImageButton btnFav;
         public CategoryViewHolder(@NonNull View view) {
             super(view);
 
             //intialize local buttons for recyclerview.
             txtName = view.findViewById(R.id.txtName);
             btnSelect = view.findViewById(R.id.btnSelect);
-            btnSelect.setBackgroundColor(Color.parseColor("#FF4CAF50"));
+            btnFav = view.findViewById(R.id.btnFav);
+            faveList = new ArrayList<>();
 
+
+            sp = view.getContext().getSharedPreferences("FAVES", Context.MODE_PRIVATE);
+
+            btnSelect.setBackgroundColor(Color.parseColor("#FF4CAF50"));
 
             //on btnselect being clicked perform the following.
             btnSelect.setOnClickListener(e->{
@@ -83,6 +123,35 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
                 startActivity(view.getContext(),i,b);
 
                 GameSounds.vineBoom(e.getContext());
+            });
+
+            btnFav.setOnClickListener(e->{
+                if(!faveList.contains(category.get(getAdapterPosition()).getId())){
+                    faveList.add(category.get(getAdapterPosition()).getId());
+                    //btnFav.setImageResource(R.drawable.h1);
+                }else{
+
+                    faveList.remove(faveList.indexOf(category.get(getAdapterPosition()).getId()));
+                    btnFav.setImageResource(R.drawable.h2);
+                }
+
+                if(faveList.contains(category.get(getAdapterPosition()).getId())){
+                    btnFav.setImageResource(R.drawable.h1);
+                }
+
+                SharedPreferences.Editor ed = sp.edit();
+                String fav = "";
+                for(int j = 0;j<faveList.size();j++){
+                    fav += faveList.get(j).toString() + " ";
+                }
+
+                //category.sort(Comparator.comparing(faveList -> faveList.getId()));
+                //notifyDataSetChanged();
+                ed.putString("fa",fav);
+                ed.apply();
+
+                Log.d("HESH",category.get(getAdapterPosition()).getId()+" "+ faveList.toString());
+
             });
 
         }
